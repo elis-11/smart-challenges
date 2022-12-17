19bcc35a9130fd8610b3323f3ddaffa6a42c947c
elis-11 <eliza.arzanukaeva@gmail.com>
2022-12-17 15:51:42 +0100

    NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Cart } from "./pages/Cart";
-import { NotFound } from "./pages/NotFound";
+import { NotFound } from "./components/notfound/NotFound.jsx";
 
 function App() {
   return (
@@ -8,9 +9,12 @@ function App() {
       <div className="flex  items-center justify-center font-bold text-4xl text-orange-300 mx-auto my-2">
         <h2 className="">Smart Challenges</h2>
       </div>
-      <Home />
-      <Cart />
-      <NotFound />
+    <Routes>
+      <Route path="" element={<Home />} />
+      {/* <Route path="/product/:id" element={<Home />} /> */}
+      <Route path="cart" element={<Cart />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
     </div>
   );
 }
